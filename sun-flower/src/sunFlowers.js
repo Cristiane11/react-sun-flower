3e8f56b85de6b6bd6fe0ae36384d89eaa644922d
Cristiane11 <bjwmcd@yahoo.com>
2020-02-01 13:39:50 -0500

    update

---
+++ sun-flower/src/sunFlowers.js
@@ -0,0 +1,31 @@
+import React from 'react';
+export const sunFlowers=[
+    {
+        id:1,
+        flower:'Sun Flower 1',
+        size:'Large',
+        location: 'United States'
+
+    },
+    {
+        id:2,
+        flower:'Sun Flower 2',
+        size:'Medium',
+        location: 'California'
+    
+    },
+    {
+        id:3,
+        flower:'Sun Flower 3',
+        size:'small',
+        location: 'New Jersey'
+    
+    },
+    {
+        id:4,
+        flower:'Sun Flower 4',
+        size:'Medium',
+        location: 'New York'
+    
+    }
+]
